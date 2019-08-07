9814a3d5dc367563d7b70f86f8bcd99c82f74df8
Alan Xavier <alan.xavier@apdynamics.local>
2019-08-06 20:01:19 -0400

    Add html-loader, file-loader, and clean-webpack-plugin
    - Added html-loader to automatically require the files we reference in img tags
    - Added file-loader to handle the svg,png,jpg,gif files from our images
    - Configured file-loader to add our images to an imgs directory in dist
    - Also configured it to add a hash to their filenames
    - Lastly, added clean-webpack-plugin to our production config to clean out the dist directory each time we build

--- webpack.common.js
+++ webpack.common.js
@@ -11,12 +11,26 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.scss$/,
+        test: /\.scss$/, // COMPILES CSS or SASS FILES
         use: [
           "style-loader", // 3. Inject styles into DOM
           "css-loader", // 2. Turns css into commonjs
           "sass-loader" // 1. Turns sass into css
         ]
+      },
+      {
+        test: /\.html$/, // HTML Loader --> when this is encountered in an .html file, <img src="image.png">, it is required (require('./image.png'))
+        use: ["html-loader"]
+      },
+      {
+        test: /\.(svg|png|jpe?g|gif)$/, // File Loader is going to use the image encountered by HTML Loader, then copies it to a folder 'imgs'
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name].[hash].[ext]",
+            outputPath: "imgs"
+          }
+        }
       }
     ]
   }
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,11 +1,13 @@
 const path = require("path");
 const common = require("./webpack.common");
 const merge = require("webpack-merge");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "production",
   output: {
     filename: "main.[contentHash].js", // Content hash prevents caching by always changing the 'main.js' build when code is updated
     path: path.resolve(__dirname, "dist")
-  }
+  },
+  plugins: [new CleanWebpackPlugin()]
 });
